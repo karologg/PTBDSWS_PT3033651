216c1d89317d5b9d39f6b86e3e2b7bfb12de03c0
karolo <karoloarchive@gmail.com>
2026-06-03 07:37:21 +0000

    seal repo :)

---
+++ atv_03/flask_app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, url_for, render_template, flash, session, redirect
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave forte'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('what is your name?', validators=[DataRequired()])
+    submit = SubmitField('submit')
+
+@app.route("/", methods=["GET", "POST"])
+def main():
+    form = NameForm()
+
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('main'))
+
+    return render_template('index.html', form=form, name=session.get('name'))
+
+# Handler para erros 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ atv_07/app/main/views.py
@@ -0,0 +1,46 @@
+from flask import render_template, session, redirect, url_for, current_app
+from .. import db
+from ..models import User, Role, EmailLog
+from ..email import send_email
+from . import main
+from .forms import NameForm
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user_role = Role.query.filter_by(name='User').first()
+            if user_role is None:
+                user_role = Role(name='User')
+                db.session.add(user_role)
+            
+            user = User(username=form.name.data, role=user_role)
+            db.session.add(user)
+            db.session.commit()
+            
+            session['known'] = False
+            
+            if current_app.config['FLASKY_ADMIN']:
+                
+                send_email(current_app.config['FLASKY_ADMIN'], 
+                           'novo usuário cadastrado',
+                           'mail/new_user', 
+                           user=user) 
+        else:
+            session['known'] = True
+        
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+        
+    return render_template('index.html', 
+                           form=form, 
+                           name=session.get('name'),
+                           known=session.get('known', False))
+
+@main.route('/emails')
+def emails():
+
+    email_list = EmailLog.query.order_by(EmailLog.timestamp.desc()).all()
+    return render_template('emails.html', emails=email_list)
